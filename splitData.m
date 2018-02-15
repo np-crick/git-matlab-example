f327c3888de1fa5fc89c398d27433d5a4708bc55
Tom <warnert@c02v23q7htdd.dhcp.thecrick.org>
2018-02-15 17:28:37 +0000

    Split mice depending on NMDA value

--- splitData.m
+++ splitData.m
@@ -1,6 +1,17 @@
 function [ control, test ] = splitData( dataSet )
 %SPLITDATA splits a standard AutonoMouse data set into two structures, one
 %for control animals, one for test animals based on NMDA parameter
-
+names = fieldnames(dataSet);
+disp(dataSet.(names{1}))
+field_names = fieldnames(dataSet.(names{1}));
+disp(field_names)
+control = struct();
+test = struct();
+for i =1:numel(names)
+    NMDA = dataSet.(names{i}).NMDA;
+    if NMDA == 0
+        test.(names{i})=dataSet.(names{i});
+    else
+        control.(names{i}) = dataSet.(names{i});
+    end
 end
-
