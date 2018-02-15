5a545483bc5b3c9b3d6f95bcf9e7e0ebcc1d1013
atranvan <alexandra.tran.van.minh@gmail.com>
2018-02-15 18:09:44 +0000

    add functions

--- binParameters.m
+++ binParameters.m
@@ -2,6 +2,10 @@ function [ binnedData ] = binParameters( parameterMatrix, binSize )
 %BINPARAMETERS Takes a parameter matrix (rows is individual animals,
 %columns is single trials) and bins with a sliding window. Each column
 %should be the average of the previous n binSize trials
-
+binnedData= nan(size(parameterMatrix,1), size(parameterMatrix,2));
+for i = binSize:length(binnedData)
+    thisBin = parameterMatrix(:, (i-binSize)+1:i);
+    binnedData(:,i) = nanmean(thisBin,2);
+end
 end
 
--- getParameters.m
+++ getParameters.m
@@ -3,5 +3,15 @@ function [ parameters ] = getParameters( dataSet, task, parameter )
 %parameters for a given trial where each row is an individual animal and
 %each column is the chosen parameter on each trial
 
+names = fieldnames(dataSet);
+
+for i=1:size(names,1)
+    mouseparameters{i}=dataSet.(names{i}).(task).(parameter);
+end
+maxLength = max(cellfun(@length, mouseparameters));
+parameters = nan(length(mouseparameters), maxLength);
+for j=size(mouseparameters,2)
+    parameters(j,1:length(mouseparameters{1,j}))=mouseparameters{1,j};
+end
 end
 
